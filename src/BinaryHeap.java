2ea956ccd69152911b479088115132c3c63f5712
wzym <wzym@yandex.ru>
2015-08-03 23:27:50 +0300

    Добавлен скелет алгоритма извлечения максимального элемента с последующей заменой на последни (самый маленький) элемент и опусканием его на дно для замещения освободившегося места.

--- src/BinaryHeap.java
+++ src/BinaryHeap.java
@@ -6,12 +6,24 @@ public class BinaryHeap<K extends Comparable<K>, T> implements PriorityQueue<K, 
 
     @Override
     public T getMax() {
-        return null;
+        if (isEmpty()) return null;
+        @SuppressWarnings("unchecked")
+        T result = (T) body[0].getMainPart();
+        body[0] = body[size - 1];
+        --size;
+        if (size > 1) {
+            setElementDown(0);
+        }
+        return result;
+    }
+
+    private void setElementDown(int indexOfElementToFall) {
+
     }
 
     @Override
     public void insert(K index, T element) {
-        body[size] = new HeapElement(index, element);
+        body[size] = new HeapElement<K, T>(index, element);
         setElementUp(size);
         ++size;
         if (size == body.length) {
