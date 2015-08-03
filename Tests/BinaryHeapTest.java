05dd88e66d260ef7ad1a8a10199f49ea99d0350f
wzym <wzym@yandex.ru>
2015-08-03 22:23:08 +0300

    Основной класс бинарной кучи и тесты для него.

---
+++ Tests/BinaryHeapTest.java
@@ -0,0 +1,18 @@
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+
+public class BinaryHeapTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+}
