4f64a2bbc713caa2fef34212b4caec96477e56b7
wzym <wzym@yandex.ru>
2015-08-03 23:11:06 +0300

    Добавлен основной тест (каждый следующий максимальный элемент кучи не больше предыдущего).

--- Tests/BinaryHeapTest.java
+++ Tests/BinaryHeapTest.java
@@ -1,18 +1,36 @@
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-
 public class BinaryHeapTest {
+    private final int ARRAY_SIZE_DIAPASON = 1000;
+    private final int MIN_ARRAY_SIZE = 13;
+    private final int MAX_ELEMENT_SIZE = 100;
 
-    @Before
-    public void setUp() throws Exception {
-
+    @Test
+    public void simpleOrderTest() {
+        Integer[] randomIntegers = getRandomIntegerArray();
+        BinaryHeap heap = new BinaryHeap<Integer, Integer>();
+        for (Integer randomInteger : randomIntegers) {
+            heap.insert(randomInteger, randomInteger);
+        }
+        Integer currentPeak = MAX_ELEMENT_SIZE + 1;
+        while (!heap.isEmpty()) {
+            Integer lastPeak = currentPeak;
+            currentPeak = heap.getMax();
+            Assert.assertTrue(lastPeak >= currentPeak);
+        }
     }
 
-    @After
-    public void tearDown() throws Exception {
-
+    private Integer[] getRandomIntegerArray() {
+        int size = (int) Math.round(Math.random() * ARRAY_SIZE_DIAPASON) + MIN_ARRAY_SIZE;
+        Integer[] result = new Integer[size];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = (int) Math.round(Math.random() * MAX_ELEMENT_SIZE);
+        }
+        return result;
     }
 }
